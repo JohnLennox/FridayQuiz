f2a89f446ea3448f64d9ce0236168f3c6948bad5
John Lennox <jl1998@hotmail.co.uk>
2022-09-05 22:30:45 +0100

    removed unused import

--- fridayquiz/src/App.js
+++ fridayquiz/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {BrowserRouter as Router, Route, Link, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 import QuizHome from "./pages/QuizHome";
 import Question from "./pages/Question";
